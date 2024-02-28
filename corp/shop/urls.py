50d6398133e5a6df30317d49d923651cbea7ce0d
nyckolas <nyckolas.python@gmail.com>
2024-02-28 20:12:25 +0200

    chore: add initial urls

---
+++ corp/shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "shop"
+
+urlpatterns = [
+    path("", views.products_view, name="products"),
+    path("<slug:slug>/", views.product_detail_view, name="product_detail"),
+    path("search/<slug:slug>/", views.category_list_view, name="category_list"),
+]
