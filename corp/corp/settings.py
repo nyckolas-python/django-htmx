762a01bb8a43d187910b40ae5df9c062b8bf0dff
nyckolas <nyckolas.python@gmail.com>
2024-02-28 20:13:38 +0200

    chore: add categories to context processors

--- corp/corp/settings.py
+++ corp/corp/settings.py
@@ -47,6 +47,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                #Custom Context Processors
+                'shop.context_processors.categories',
             ],
         },
     },
