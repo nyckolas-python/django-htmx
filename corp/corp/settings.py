2d85d3d6a10a6fadd3ea8e103c28d84143f7a75d
nyckolas <nyckolas.python@gmail.com>
2024-02-28 10:06:28 +0200

    feature: add corp project, add shop app, add Category, Product, ProductManager, ProductProxy models

---
+++ corp/corp/settings.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-db0swg__(z!#fk6gvu%w89+%md@qi3jc14n+)$wagbl92cj@td'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # apps
+    'shop.apps.ShopConfig',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'corp.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'corp.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ corp/shop/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.utils.text import slugify
+import random
+import string
+from django.urls import reverse
+
+
+def rand_slug():
+    """
+    Generate a random slug consisting of a combination of three alphanumeric characters.
+
+    Example:
+    rand_slug() -> 'aB3'
+    """
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(3))
+
+
+class Category(models.Model):
+    """
+    This model represents a category for products.
+    """
+    name = models.CharField(verbose_name='Category', max_length=255, db_index=True)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, related_name='children', null=True, blank=True
+    )
+    slug = models.SlugField(verbose_name='URL', max_length=255, unique=True, null=False, editable=True)
+
+    created_at = models.DateTimeField(verbose_name='Date of creation', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Date of update', auto_now=True)
+
+    class Meta:
+        unique_together = ('slug', 'parent')
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        """
+        Return a string representing the full path of the current node, including all parent nodes.
+        """
+        full_path = [self.name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1])
+
+    def save(self, *args, **kwargs):
+        """
+        Save the object with a generated slug if it does not already have one.
+        """
+        if not self.slug:
+            self.slug = slugify(rand_slug() + '-pickBetter' + self.name)
+
+        return super(Category, self).save(*args, **kwargs)
+
+    # def get_absolute_url(self):
+    #     return reverse('shop:category', kwargs={'slug': self.slug})
+
+
+class Product(models.Model):
+    """
+    Model representing a product.
+    """
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(verbose_name='Product', max_length=255, db_index=True)
+    brand = models.CharField(verbose_name='Brand', max_length=255, null=False, editable=True)
+    description = models.TextField(verbose_name='Description', null=True, blank=True)
+    slug = models.SlugField(verbose_name='URL', max_length=255, unique=True, null=False, editable=True)
+    price = models.DecimalField(verbose_name='Price', max_digits=10, decimal_places=2, default=99.99)
+    image = models.ImageField(verbose_name='Image', upload_to='products/products/%Y/%m/%d', null=True, blank=True)
+    available = models.BooleanField(verbose_name='Available', default=True)
+
+    created_at = models.DateTimeField(verbose_name='Date of creation', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Date of update', auto_now=True)
+
+    class Meta:
+        unique_together = ('slug', 'category')
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse("model_detail", kwargs={"pk": self.pk})
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        """
+        Method to get the queryset with a filter for available products.
+        """
+        return super(ProductManager, self).get_queryset().filter(available=True)
+
+
+class ProductProxy(Product):
+
+    objects = ProductManager()
+
+    class Meta:
+        proxy = True
+
+    def __str__(self):
+        return self.title
