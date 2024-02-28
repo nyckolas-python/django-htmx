6ce1a1e057e61950a4a2711afc18c9d16d4c565c
nyckolas <nyckolas.python@gmail.com>
2024-02-28 19:41:03 +0200

    chore: add CategoryAdmin, ProductAdmin

--- corp/shop/admin.py
+++ corp/shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent", "slug")
+    ordering = ("slug",)
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "brand", "category", "slug", "price", "available", "created_at", "updated_at")
+    list_filter = ("available", "created_at", "updated_at")
+    ordering = ("title",)
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ("title",)}
