003294518d8c940fcc533f77a29ff89c936fbfbf
nyckolas <nyckolas.python@gmail.com>
2024-03-16 16:13:48 +0200

    feature: add urls for password reset, confirm and password reset complete

--- corp/account/urls.py
+++ corp/account/urls.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -21,4 +22,24 @@ urlpatterns = [
     path('dashboard/', views.dashboard_user, name='dashboard'),
     path('profile-management/', views.profile_user, name='profile-management'),
     path('delete-user/', views.delete_user, name='delete-user'),
+
+    # Overrides templates for password reset, confirm and password reset complete
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password/password-reset.html',
+        email_template_name='account/password/password_reset_email.html',
+        success_url=reverse_lazy('account:password_reset_done')),
+        name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password/password-reset-done.html'),
+        name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='account/password/password-reset-confirm.html',
+        success_url=reverse_lazy('account:password_reset_complete')),
+        name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='account/password/password-reset-complete.html'),
+        name='password_reset_complete'),
 ]
