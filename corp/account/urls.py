6fa5990dd4bff874e9ebc8c52bf9ec021849f0bf
nyckolas <nyckolas.python@gmail.com>
2024-03-16 14:14:02 +0200

    chore: add dashboard, profile-management, delete-user to account urls

--- corp/account/urls.py
+++ corp/account/urls.py
@@ -6,10 +6,19 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+    # Registration & Verification
     path('register/', views.register_user, name='register'),
     path('email-verification-sent/',
-         lambda request: render(request, 'account/email/email-verification-sent.html'),
-         name='email-verification-sent'),
+        lambda request:render(request, 'account/email/email-verification-sent.html'), 
+        name='email-verification-sent'
+        ),
+
+    # Login & Logout
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+
+    # Dashboard
+    path('dashboard/', views.dashboard_user, name='dashboard'),
+    path('profile-management/', views.profile_user, name='profile-management'),
+    path('delete-user/', views.delete_user, name='delete-user'),
 ]
