3f76f657bbc3ebab6714ea53e73def474926c526
nyckolas <nyckolas.python@gmail.com>
2024-03-16 15:22:08 +0200

    feature: add checkout, shipping, complete_order, payment_success, payment_failed views

--- corp/payment/views.py
+++ corp/payment/views.py
@@ -1,22 +1,87 @@
-from django.shortcuts import render
+from cart.cart import Cart
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import ShippingAddressForm
+from .models import Order, OrderItem, ShippingAddress
 
 
 def checkout(request):
-    # return render(request, 'payment/checkout.html', {})
-    pass
+    if request.user.is_authenticated:
+        shipping_address = ShippingAddress.objects.filter(user=request.user)
+        if shipping_address:
+            return render(request, 'payment/checkout.html', {'shipping_address': shipping_address})
+    return render(request, 'payment/checkout.html')
 
+
+@login_required(login_url='account:login')
 def shipping(request):
-    # return render(request, 'payment/shipping.html', {})
-    pass
+    try:
+        shipping_address = ShippingAddress.objects.get(user=request.user)
+    except ShippingAddress.DoesNotExist:
+        shipping_address = None
+    form = ShippingAddressForm(instance=shipping_address)
+
+    if request.method == 'POST':
+        form = ShippingAddressForm(request.POST, instance=shipping_address)
+        if form.is_valid():
+            shipping_address = form.save(commit=False)
+            shipping_address.user = request.user
+            shipping_address.save()
+            return redirect('account:dashboard')
+
+    return render(request, 'shipping/shipping.html', {'form': form})
+
 
 def complete_order(request):
-    # return render(request, 'payment/complete-order.html', {})
-    pass
+    if request.POST.get('action') == 'payment':
+        full_name = request.POST.get('name')
+        email = request.POST.get('email')
+        street_address = request.POST.get('street_address')
+        apartment_address = request.POST.get('apartment_address')
+        country = request.POST.get('country')
+        zipcode = request.POST.get('zipcode')
+
+        cart = Cart(request)
+        total_price = cart.get_total_price()
+
+        shipping_address, _ = ShippingAddress.objects.get_or_create(
+            user=request.user,
+            defaults={
+                'full_name': full_name,
+                'email': email,
+                'street_address': street_address,
+                'apartment_address': apartment_address,
+                'country': country,
+                'zip': zipcode
+            }
+        )
+
+    if request.user.is_authenticated:
+        order = Order.objects.create(
+            user=request.user, shipping_address=shipping_address, amount=total_price)
+
+        for item in cart:
+            OrderItem.objects.create(
+                order=order, product=item['product'], price=item['price'], quantity=item['qty'], user=request.user)
+    else:
+        order = Order.objects.create(
+            shipping_address=shipping_address, amount=total_price)
+
+        for item in cart:
+            OrderItem.objects.create(
+                order=order, product=item['product'], price=item['price'], quantity=item['qty'])
+
+    return JsonResponse({'success': True})
+
 
 def payment_success(request):
-    # return render(request, 'payment/payment-success.html', {})
-    pass
+    for key in list(request.session.keys()):
+        if key == 'session_key':
+            del request.session[key]
+    return render(request, 'payment/payment-success.html')
+
 
 def payment_failed(request):
-    # return render(request, 'payment/payment-failed.html', {})
-    pass
+    return render(request, 'payment/payment-failed.html')
