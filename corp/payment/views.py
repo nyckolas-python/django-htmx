8637376413e244de06691ee49cb0151518e49fee
nyckolas <nyckolas.python@gmail.com>
2024-03-18 15:26:28 +0200

    feature: add stripe payment method to complete_order view

--- corp/payment/views.py
+++ corp/payment/views.py
@@ -1,10 +1,19 @@
+import uuid
+from decimal import Decimal
+
+import stripe
 from cart.cart import Cart
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
 from .forms import ShippingAddressForm
 from .models import Order, OrderItem, ShippingAddress
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_version = settings.STRIPE_API_VERSION
 
 
 def checkout(request):
@@ -35,7 +44,7 @@ def shipping(request):
 
 
 def complete_order(request):
-    if request.POST.get('action') == 'payment':
+    if request.method == 'POST':
         full_name = request.POST.get('name')
         email = request.POST.get('email')
         street_address = request.POST.get('street_address')
@@ -58,22 +67,51 @@ def complete_order(request):
             }
         )
 
-    if request.user.is_authenticated:
-        order = Order.objects.create(
-            user=request.user, shipping_address=shipping_address, amount=total_price)
+        if request.user.is_authenticated:
+            order = Order.objects.create(
+                user=request.user, shipping_address=shipping_address, amount=total_price)
 
-        for item in cart:
-            OrderItem.objects.create(
-                order=order, product=item['product'], price=item['price'], quantity=item['qty'], user=request.user)
-    else:
-        order = Order.objects.create(
-            shipping_address=shipping_address, amount=total_price)
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order, product=item['product'], price=item['price'], quantity=item['qty'], user=request.user)
+        else:
+            order = Order.objects.create(
+                shipping_address=shipping_address, amount=total_price)
 
-        for item in cart:
-            OrderItem.objects.create(
-                order=order, product=item['product'], price=item['price'], quantity=item['qty'])
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order, product=item['product'], price=item['price'], quantity=item['qty'])
 
-    return JsonResponse({'success': True})
+        payment_type = request.POST.get('stripe-payment', 'fondy-payment')
+
+        match payment_type:
+            case "stripe-payment":
+
+                session_data = {
+                    'mode': 'payment',
+                    'success_url': request.build_absolute_uri(reverse('payment:payment-success')),
+                    'cancel_url': request.build_absolute_uri(reverse('payment:payment-failed')),
+                    'line_items': []
+                }
+
+                for item in cart:
+                    session_data['line_items'].append({
+                        'price_data': {
+                            'unit_amount': int(item['price'] * Decimal(100)),
+                            'currency': 'usd',
+                            'product_data': {
+                                'name': item['product']
+                            },
+                        },
+                        'quantity': item['qty'],
+                    })
+
+                session = stripe.checkout.Session.create(**session_data)
+                return redirect(session.url, code=303)
+
+            # TODO: add fondy payment
+            case "fondy-payment":
+                pass
 
 
 def payment_success(request):
