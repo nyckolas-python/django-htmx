328d18b6842eb7ba1ce92a7ae7ec3ba1572a6cc2
nyckolas <nyckolas.python@gmail.com>
2024-03-23 16:05:18 +0200

    feature: add paid field for Order model

--- corp/payment/models.py
+++ corp/payment/models.py
@@ -24,7 +24,9 @@ class ShippingAddress(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE, blank=True, null=True)
+
     amount = models.DecimalField(max_digits=9, decimal_places=2)
+    paid = models.BooleanField(default=False)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
