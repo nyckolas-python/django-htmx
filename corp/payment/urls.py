927c9cf6dddff9991a9e0d0626f342cead1ff5da
nyckolas <nyckolas.python@gmail.com>
2024-03-23 16:10:47 +0200

    feature: add stripe webhooks to urls

--- corp/payment/urls.py
+++ corp/payment/urls.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import path
 
-from . import views
+from . import views, webhooks
 
 app_name = 'payment'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('complete-order/', views.complete_order, name='complete-order'),
     path('payment-success/', views.payment_success, name='payment-success'),
     path('payment-failed/', views.payment_failed, name='payment-failed'),
+    path('webhook-stripe/', webhooks.stripe_webhook, name='webhook-stripe'),
 ]
