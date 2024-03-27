9c42c18ead23a51ab082cfbd279f5cfa7fb5baaf
nyckolas <nyckolas.python@gmail.com>
2024-03-27 17:48:21 +0200

    feature: add ability to search for products by title in navbar

--- corp/shop/urls.py
+++ corp/shop/urls.py
@@ -7,6 +7,7 @@ app_name = 'shop'
 urlpatterns = [
     # path('', products_view, name='products'),
     path('', views.ProductListView.as_view(), name='products'),
+    path('search_products/', views.search_products, name='search-products'),
     path('<slug:slug>/', views.product_detail_view, name='product-detail'),
     path('search/<slug:slug>/', views.category_list_view, name='category-list'),
 ]
--- corp/shop/views.py
+++ corp/shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView
 
 from .models import Category, ProductProxy
@@ -18,12 +20,24 @@ class ProductListView(ListView):
             return "shop/components/product_list.html"
         return "shop/products.html"
 
+
 def product_detail_view(request, slug):
     product = get_object_or_404(ProductProxy, slug=slug)
     return render(request, 'shop/product_detail.html', {'product': product})
+
 
 def category_list_view(request, slug):
     category = get_object_or_404(Category, slug=slug)
     product = ProductProxy.objects.select_related('category').filter(category=category)
     context = {'category': category, 'products': product}
     return render(request, 'shop/category_list.html', context=context)
+
+def search_products(request: HttpRequest) -> HttpResponse:
+    query = request.GET.get('q')
+    if not query:
+        return redirect('shop:products')
+
+    products = ProductProxy.objects.filter(title__icontains=query).distinct()
+    context = {'products': products, 'searching_query': query}
+
+    return render(request, 'shop/products.html', context)
